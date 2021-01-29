804ae0bdb079b03ba110170eb92f46b2ccf7fe9a
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-29 22:59:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,8 @@ end
   library = load_library(file)
   boob = library.keys.find do |key|
     library[key][:japanese] == value 
-    binding.pry 
   end 
+  binding.pry 
   boob ? boob : "Sorry, that emoticon was not found"
 end 
 
