690ff532268a51886ce9058634f450645f9dd629
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-29 22:48:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,10 +15,10 @@ end
   def get_english_meaning(file, value)
   library = load_library(file)
   binding.pry 
-  value = library.keys.find do |key|
+  boob = library.keys.find do |key|
     library[key][:japanese] == value 
   end 
-  value ? value : "Sorry, that emoticon was not found"
+  value ? boob : "Sorry, that emoticon was not found"
 end 
 
 
